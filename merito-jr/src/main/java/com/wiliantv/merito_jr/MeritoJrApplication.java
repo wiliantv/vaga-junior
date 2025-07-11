376b84e24404d6b42521a4186417f1c5bb8fd210
Wilian Dorneles <59717485+wiliantv@users.noreply.github.com>
2025-07-11 09:40:20 -0300

    feat: Inicializa projeto merito-jr

---
+++ merito-jr/src/main/java/com/wiliantv/merito_jr/MeritoJrApplication.java
@@ -0,0 +1,13 @@
+package com.wiliantv.merito_jr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeritoJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeritoJrApplication.class, args);
+	}
+
+}
